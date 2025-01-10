53833e8af7e46173470a728d3a49907d5d5aec53
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-10 21:14:44 +0900

    SetHealth SetAttack

---
+++ TextRPG/Character.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Character
+{
+private:
+    static Character* Instance;
+    string Name;
+    int Level;
+    int MaxLevel;
+    double Health; // 체력
+    double MaxHealth; // 최대 체력
+    double Attack; // 공격력
+    double Experience; // 경험치
+    double MaxExperience; // 최대 경험치
+    double Gold; // 돈
+
+    Character(string name);
+
+public:
+    static Character* GetInstance(string name = "");
+    string GetName();
+    int GetLevel();
+    double GetHealth();
+    double GetAttack();
+    void SetHealth(double health);
+    void SetAttack(double attack);
+    void TakeDamage(double damage);
+    double GetExperience();
+    double GetMaxExperience();
+    double GetGold();
+};
